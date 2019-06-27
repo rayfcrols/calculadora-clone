f7990f85c5ff0b8a064c58cd452f030f887c54a7
Raimundo Luiz Oliveira <rayfcrols@gmail.com>
2019-06-27 18:01:00 -0300

    Commit 1

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,46 @@
+class CalcController {
+
+    constructor() {
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize() {
+
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayCalc() {
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate() {
+        return new Date().toLocaleDateString("pt-BR")
+    }
+
+    set currentDate(value) {
+        this._currentDate = value;
+    }
+}
