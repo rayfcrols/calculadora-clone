a880da5d7e9d81c85624c7aa6b189c139a4dcecc
Raimundo Luiz Oliveira <rayfcrols@gmail.com>
2019-06-29 13:10:34 -0300

    Commit 1

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -7,14 +7,44 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonEvents();
     }
 
     initialize() {
+        this.setDisplayDateTime();
         setInterval(() => {
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale);    
-        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);    
+            this.setDisplayDateTime();
         }, 1000);
 
+
+    }
+
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+
+    initButtonEvents() {
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index) => {
+            this.addEventListenerAll(btn, "drag click", e => {
+                console.log(btn.className.baseVal.replace("btn-",""));
+            });
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer";
+            });
+        });
+
+
+    }
+    
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, { day: "2-digit", month: "long", year: "numeric" });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
     get displayDate() {
