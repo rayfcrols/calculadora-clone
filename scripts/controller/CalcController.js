27d8258cd2e151d0f89e0b8333963b7ace700986
Raimundo Luiz <rayfcrols@gmail.com>
2019-07-20 22:57:28 -0300

    Commit 1

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,7 +1,8 @@
 class CalcController {
 
     constructor() {
-        this._locale = "pt-BR"
+        this._operation = [];
+        this._locale = "pt-BR";
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
@@ -25,14 +26,78 @@ class CalcController {
         });
     }
 
+    clearAll(){
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+
+    addOperation(value){
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value){
+        switch (value){
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+                this.clearEntry();
+                break;
+            case 'subtracao':
+                this.clearEntry();
+                break;
+            case 'divisao':
+                this.clearEntry();
+                break;
+            case 'multiplicacao':
+                this.clearEntry();
+                break;
+            case 'porcento':
+                this.clearEntry();
+                break;
+            case 'igual':
+                this.clearEntry();
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+                break;
+        }
+
+    }
 
     initButtonEvents() {
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index) => {
-            this.addEventListenerAll(btn, "drag click", e => {
-                console.log(btn.className.baseVal.replace("btn-",""));
+            this.addEventListenerAll(btn, "click drag", e => {
+                //console.log(btn.className.baseVal.replace("btn-", ""));
+               let textBtn = btn.className.baseVal.replace("btn-", "");
+               this.execBtn(textBtn);
             });
+
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
                 btn.style.cursor = "pointer";
             });
